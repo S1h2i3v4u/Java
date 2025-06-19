764b6569125ef46ef4a4aca0cb2e06c8ab69f052
Shivani Suryawansi <158851522+S1h2i3v4u@users.noreply.github.com>
2025-06-19 22:01:18 +0530

    Create Grade_Card

---
+++ Grade_Card.java
@@ -0,0 +1,52 @@
+package mit;
+import java.util.*;
+public class Grade {
+
+	public static void main(String[] args) {
+		// Grade Card
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Enter the name : ");
+		String name = sc.nextLine();
+		
+		System.out.print("Enter the marks of Math :");
+		int math = sc.nextInt();
+		System.out.print("Enter the marks of Bio :");
+		int Bio = sc.nextInt();
+		System.out.print("Enter the marks of Phy :");
+		int Phy = sc.nextInt();
+		System.out.print("Enter the marks of CC :");
+		int CC = sc.nextInt();
+		System.out.print("Enter the marks of Java :");
+		int java = sc.nextInt();
+		
+		System.out.println("===Grade Card===");
+		System.out.println("Name :" +name);
+		
+		int total = math+Bio+Phy+CC+java;
+		System.out.println("Total Marks is : " + total);
+		
+		int percentage = total/5;
+		System.out.println("Percentage is : " + percentage+"%");
+		
+	
+		if (percentage >= 90) {
+            System.out.println("Grade: A+");
+        } else if (percentage >= 80) {
+            System.out.println("Grade: A");
+        } else if (percentage >= 70) {
+            System.out.println("Grade: B");
+        } else if (percentage >= 60) {
+            System.out.println("Grade: C");
+        } else if (percentage >= 50) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F (Fail)");
+        }
+		}
+		
+
+	}
+
+
